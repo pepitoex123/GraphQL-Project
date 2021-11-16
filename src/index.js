bd26fc205a85a35325fb3d39434451d2d7a10f5f
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-16 16:21:35 -0300

    added first graphql lines of code!

---
+++ src/index.js
@@ -0,0 +1,33 @@
+import { GraphQLServer } from "graphql-yoga";
+
+
+// Type Definitions (application schema)
+
+const typeDefs = `
+    type Query {
+        hello: String!    
+    }
+`
+
+// Resolvers
+
+const resolvers = {
+    Query: {
+        hello() {
+            return "This is my first query!"
+        }
+    }
+}
+
+
+const server = new GraphQLServer({
+    typeDefs,
+    resolvers
+})
+
+server.start(() => {
+    console.log("The server is up!");
+})
+
+console.log("Hello GraphQL")
+
