216bef5f82abe65af36f4815a8945b139d7ea1c9
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-11-17 18:06:43 -0300

    added createUser mutation

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import { GraphQLServer } from "graphql-yoga";
+import uuidv4 from "uuid/v4";
 
 
 // Scalar Types: --- String, Boolean, Int, Float, ID ---
@@ -75,6 +76,10 @@ const typeDefs = `
         comments: [Comment!]!
     }
     
+    type Mutation {
+        createUser(name: String!, email: String!, age: Int): User!
+    }
+    
     type User {
         id: ID!
         name: String!
@@ -132,6 +137,22 @@ const resolvers = {
         },
         comments(parent,args,ctx,info){
             return comments
+        }
+    },
+    Mutation: {
+        createUser(parent,args,ctx,info) {
+            const emailTaken = users.some((user) => user.email === args.email)
+            if(emailTaken){
+                throw new Error(`The email ${args.email} has already been taken`)
+            }
+            const user = {
+                id: uuidv4(),
+                ...args
+            }
+
+            users.push(user)
+
+            return user
         }
     },
     Post: {
