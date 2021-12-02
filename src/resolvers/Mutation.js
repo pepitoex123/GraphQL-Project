92dc563feeb2822915d9b6ca5c68b09ba47f2fd9
Uriel Cabrera <urimartincabrera123@gmail.com>
2021-12-01 21:23:14 -0300

    added comment subscriptions

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -170,20 +170,29 @@ const Mutation = {
 
         db.comments.push(comment)
         pubsub.publish(`comment ${args.data.post}`, {
-            comment
+            comment: {
+                mutation: "CREATED",
+                data: comment
+            }
         })
 
         return comment;
     },
-    deleteComment(parent,args,{db},info){
+    deleteComment(parent,args,{db,pubsub},info){
         let commentIndex = db.comments.findIndex((comment) => comment.id === args.id);
         if(!commentIndex){
             throw new Error(`The comment with id ${args.id} does not exist`);
         }
         let deletedComment = db.comments.splice(commentIndex,1);
+        pubsub.publish(`comment ${args.data.post}`, {
+            comment: {
+                mutation: "DELETED",
+                data: deletedComment[0]
+            }
+        })
         return deletedComment[0];
     },
-    updateComment(parent,{id,data},{db},info){
+    updateComment(parent,{id,data},{db,pubsub},info){
         const comment = db.comments.find((comment) => comment.id === id)
         if(!comment){
             throw new Error("Comment not found!");
@@ -191,6 +200,14 @@ const Mutation = {
         if(typeof data.text === "string"){
             comment.text = data.text
         }
+
+        pubsub.publish(`comment ${data.post}`, {
+            comment: {
+                mutation: "UPDATED",
+                data: comment 
+            }
+        })
+
         return comment
     }
 
